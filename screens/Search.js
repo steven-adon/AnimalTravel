098906f5d605882e50f0caf6dedc6280e76e0d13
yudong <you@gmail.com>
2020-01-31 15:43:44 +0800

    commenting and search

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '../styles.js'
+import styles from '../styles'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { View, Text, SafeAreaView, TextInput, FlatList, Image, TouchableOpacity } from 'react-native';
+import db from '../config/firebase';
+import { getUser } from '../actions/user'
 
-export default class Search extends React.Component {
+class Search extends React.Component {
+    state = {
+        search: '',
+        query: []
+    }
+
+    searchUser = async () => {
+        let search = []
+        const query = await db.collection('users').where('username', '>=', this.state.search).get()
+        query.forEach((response) => {
+            search.push(response.data())
+        })
+        this.setState({ query: search })
+    }
+
+    goToUser = (user) => {
+		console.log(user)
+		this.props.getUser(user.uid)
+		this.props.navigation.navigate('Profile')
+	}
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text> Search </Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(search) => this.setState({ search })}
+                    value={this.state.search}
+                    returnKeyType='send'
+                    placeholder='Search'
+                    onSubmitEditing={this.searchUser} />
+                <FlatList
+                    data={this.state.query}
+                    keyExtractor={(item) => JSON.stringify(item.date)}
+                    renderItem={({ item }) => (
+                        <View style={[styles.row, styles.space]}>
+                            <Image style={styles.roundImage} source={{ uri: item.photo }} />
+                            <TouchableOpacity onPress={() => this.goToUser(item)} style={[styles.container, styles.left]}>
+                                <Text style={styles.bold}>{item.username}</Text>
+                                <Text style={styles.gray}>{item.bio}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )} />
+            </SafeAreaView>
         );
     }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({ getUser }, dispatch)
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
